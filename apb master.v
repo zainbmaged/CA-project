9520402796088900943d71b878a2079088c6e372
ReemHassan578 <104118223+ReemHassan578@users.noreply.github.com>
2022-12-27 17:24:49 +0200

    Update apb master.v

--- apb master.v
+++ apb master.v
@@ -1,6 +1,6 @@
 module apbmaster(
 input 		pclk,preset,pready,
-input 		Read_write,//01-read,11-write
+input 	[1:0]	Read_write,//01-read,11-write
 output reg	psel,
 output reg	penable,
 output 	  [31:0]paddr,
@@ -9,13 +9,14 @@ output 		[31:0]pwdata,
 input  		[31:0]prdata
     );
     
+
 reg [31:0]cur_pwrite,nex_pwrite,cur_prdata,nex_prdata;
 reg [1:0] cur_s,nex_s;
 parameter idle = 2'b00,setup = 2'b01,access = 2'b10;
 
 always @(posedge pclk)
 begin
-	if(~presetn)
+	if(~preset)
 	begin
 		cur_s <= idle;
 		cur_pwrite <= 32'b0;
@@ -70,11 +71,11 @@ begin
 	endcase
 end
 
-assign paddr = (cur_s == access)?8'h32:8'h0000;
+assign paddr = (cur_s == access)?32'h32:32'h0000;
 assign pwrite = cur_pwrite;
-assign pwdata = (cur_s == access)?cur_prdata:7'b0;
+assign pwdata = (cur_s == access)?cur_prdata:32'b0;
 
 
 endmodule
     
-    
+
