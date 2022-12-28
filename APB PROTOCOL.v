963e4f7256655e1c2e766761745c58358d251b65
NadaWaleed598 <104146898+NadaWaleed598@users.noreply.github.com>
2022-12-28 18:01:30 +0400

    edit

--- APB PROTOCOL.v
+++ APB PROTOCOL.v
@@ -1,23 +1,25 @@
-//`include " uart .v"
 `include "gpio.v"
 `include "apbmaster.v"
 
 module apb_prptocol(input PCLK,
                input PRESETn,
                input Transfer,
-               input [1:0] Wr_Rd,
+               input  Read_Write,
                input [31:0] apb_write_paddr,
 		           input [31:0] apb_write_data,
 		           input [31:0] apb_read_paddr,
-              output [31:0] read_data);
+              output [31:0] read_dataout);
                
-  
-    wire [32:0] PADDR;
+    wire [31:0] PADDR;
     wire [31:0] PWDATA,PRDATA;
-    wire PSELx,PENABLE,PWRITE,PREADY;
-	
-    apbmaster Design_master (Transfer,Wr_Rd [1:0],Address,write_data,read_data,PCLK,
-    PRESETn,PREADY,PRDATA,PSLVERR,PADDR,PSELx,PENABLE,PWRITE,PWDATA);
-    gpio_slave Design_slave(PCLK,PRESETn,PADDR,PSELx,PENABLE,PWRITE,PWDATA,PREADY,PRDATA);
+    wire PSEL1,PENABLE,PWRITE,PREADY, PSlavErr;
+    
+    apbmaster Design_master (PCLK , PRESETn,PREADY,Transfer, Read_Write, PSELx, PENABLE, PADDR,
+     PWRITE, PWDATA , read_dataout, PRDATA,  apb_write_data,
+     apb_write_paddr, apb_read_paddr ,PSlavErr);
+     
+     gpio_apb  Design_slave(PCLK,PRESETn, PADDR, PSEL1,PENABLE,PWRITE,
+    PWDATA, PREADY ,PRDATA , PSlavErr);
+    
 endmodule
 
