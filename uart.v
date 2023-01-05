a5efaa9f331af9150da62dd164f0501d26c3d42f
Menna-Elkhwass <105253678+Menna-Elkhwass@users.noreply.github.com>
2023-01-05 23:13:03 +0200

    Update uart.v

--- uart.v
+++ uart.v
@@ -8,7 +8,7 @@
 module uart
    #( // Default setting:
       // 19,200 baud, 8 data bits, 1 stop bit, 2^2 FIFO
-      parameter DBIT = 32,     // # data bits
+      parameter DBIT = 8,     // # data bits
                 SB_TICK = 16, // # ticks for stop bits, 16/24/32
                               // for 1/1.5/2 stop bits
                 DVSR = 163,   // baud rate divisor
@@ -20,9 +20,9 @@ module uart
    (
     input wire clk, reset,
     input wire rd_uart, wr_uart, rx,
-      input wire [31:0] w_data,
+      input wire [7:0] w_data,
     output wire tx_full, rx_empty, tx,
-      output wire [31:0] r_data
+      output wire [7:0] r_data
    );
 
    // signal declaration
